11244da16990c8d6f1c2e8e76b69096505836454
Alix <dehaut.alix@gmail.com>
2019-07-05 00:09:35 +0200

    mail sending on register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Mail\RegisterConfirmation;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -63,6 +65,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        Mail::to($data['email'])->send(new RegisterConfirmation($data));
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
