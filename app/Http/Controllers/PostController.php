b847472f89d195d5872d046bd90971bc7c66a281
Alix <dehaut.alix@gmail.com>
2019-07-05 11:58:59 +0200

    [ADD] Post feature - Stable version

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Category;
 
 class PostController extends Controller
 {
     public function post(){
-        return view('posts.post');
+
+
+        $categories = Category::all();
+
+        $posts = Post::all();
+
+
+
+        return view('posts.post',['categories' => $categories,'posts' => $posts]);
+    }
+
+
+    public function addPost(Request $request){
+        $this->validate($request,[
+            'post_body' => 'required',
+            'post_title' => 'required',
+            'category_id' => 'required'
+
+        ]);
+
+        $post = new Post();
+        $post->post_body = $request->input('post_body');
+        $post->post_title = $request->input('post_title');
+        $post->category_id = $request->input('category_id');
+        $post->post_image = 'http://localhost/uploads/b54098375f1328cc59702b21f100f22e.jpg';
+        $post->user_id = Auth::user()->getAuthIdentifier();
+
+        $post->save();
+        return redirect('/post')->with('response','Post Added Successfully');
     }
 }
--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -14,7 +14,78 @@
                             </div>
                         @endif
 
-                        List of Posts
+                        @foreach($posts as $post)
+                            <h1>{{$post->post_title}}</h1>
+                            <h3>{{$post->post_body}}</h3>
+                            <p>{{$post->category->category}}</p>
+                            <img src="{{$post->post_image}}">
+                        @endforeach
+
+                            <form method="POST" action="{{ url('/addPost') }}">
+                                @csrf
+                                @if(count($errors) > 0)
+                                    @foreach($errors->all as $error)
+                                        <div class="alert alert-danger">{{$error}}</div>
+                                    @endforeach
+                                @endif
+
+                                @if(session('response'))
+                                    <div class="alert alert-success">{{session('response')}}</div>
+                                @endif
+
+                                <div class="form-group row">
+                                    <label for="post_title" class="col-md-4 col-form-label text-md-right"> Post title</label>
+                                    <div class="col-md-6">
+                                        <input id="post_title" type="text" class="form-control @error('post_title') is-invalid @enderror" name="post_title" value="{{ old('post_title') }}" required autocomplete="post_title" autofocus>
+
+                                        @error('post_title')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group row">
+                                    <label for="post_body" class="col-md-4 col-form-label text-md-right"> Post body</label>
+                                    <div class="col-md-6">
+                                        <input id="post_body" type="text" class="form-control @error('post_body') is-invalid @enderror" name="post_body" value="{{ old('post_body') }}" required autocomplete="post_body" autofocus>
+
+                                        @error('post_title')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group row">
+                                    <label for="category_id" class="col-md-4 col-form-label text-md-right"> Post body</label>
+                                    <div class="col-md-6">
+                                        <select id="category_id" class="form-control @error('category_id') is-invalid @enderror" name="category_id" value="{{ old('category_id') }}" required autocomplete="category_id" autofocus>
+                                            @foreach($categories as $category)
+                                                <option value="{{$category->id}}">{{$category->category}}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('category_id')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-8 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            Add
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                     </div>
                 </div>
             </div>
